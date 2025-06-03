5aa0e946a587649c4eaf48c1b7c690e92172fb0c
GuillermoMancilla <g.andres.mancilla@gmail.com>
2025-06-02 23:59:38 -0400

    modificacion para funcionamiento batch
    se comenta el generated value para probar funcionamiento con spring batch

--- src/main/java/com/technova/shopverse/model/Product.java
+++ src/main/java/com/technova/shopverse/model/Product.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 @Entity
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
